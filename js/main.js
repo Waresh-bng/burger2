9f73b5b3c20f3ad12a92cb9889c06dc3bfa64fed
Waresh-bng <61429971+Waresh-bng@users.noreply.github.com>
2022-04-20 13:12:56 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// project testimonial slider code
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
